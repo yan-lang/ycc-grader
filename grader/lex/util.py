8a767b7c8e48964050a99bb25f3420ff7dbaeb6d
Zeqiang-Lai <916323301@qq.com>
2020-04-26 21:42:42 +0800

    fix: add missing helper function

--- grader/lex/util.py
+++ grader/lex/util.py
@@ -1,3 +1,11 @@
+import json
+
+
 def remove_extension(path: str):
     parts = path.split('.')
     return '.'.join(parts[:-1])
+
+
+def load_json(path: str):
+    with open(path, 'r') as f:
+        return json.load(f)
